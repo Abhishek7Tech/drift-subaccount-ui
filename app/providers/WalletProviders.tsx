2e2506cc4ce5825e3bb241f69ad665cbb487d210
Abhishek7Tech <abhishektab45@gmail.com>
2025-04-09 11:50:39 +0530

    integrated wallet

---
+++ app/providers/WalletProviders.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { FC, ReactNode } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import { clusterApiUrl } from "@solana/web3.js";
+
+import { useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { useWallet } from "@solana/wallet-adapter-react";
+
+const NETWORK_URL = process.env.NEXT_PUBLIC_NETWORK_URL;
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const network = WalletAdapterNetwork.Mainnet;
+  console.log("NETWORK", network);
+
+  //initiate auto connect
+  const { autoConnect } = useWallet();
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  //wallets
+  const wallets = useMemo(() => [], [network]);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
