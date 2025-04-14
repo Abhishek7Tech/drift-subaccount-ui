2a48d3828ef9dc95d725c53e4c065887c66dc804
Abhishek7Tech <abhishektab45@gmail.com>
2025-04-14 12:32:39 +0530

    update deposite form api route and ui

--- app/ui/deposit/deposite.tsx
+++ app/ui/deposit/deposite.tsx
@@ -16,6 +16,14 @@ import { Input } from "@/components/ui/input";
 import { useContext, useState } from "react";
 import { useStore } from "zustand";
 import useClientStore from "@/app/store/clientStore";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { ClientContext } from "@/app/providers/ClientProvider";
 import {
   Select,
@@ -47,7 +55,7 @@ const DepositeForm = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       amount: 0,
-      accountId: 0
+      accountId: 0,
     },
   });
   console.log(clientContext?.subIds);
@@ -57,7 +65,7 @@ const DepositeForm = () => {
     console.log("Amount", amount, accountId);
 
     try {
-      const req = await fetch("/api/deposite", {
+      const req = await fetch("/api/transactions/deposite", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -81,69 +89,87 @@ const DepositeForm = () => {
 
   return (
     <>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-8 min-w-2xs mx-auto bg-gray-50 rounded-xl shadow-gray-500 border shadow-sm p-4"
-        >
-          <FormField
-            control={form.control}
-            name="amount"
-            render={({ field }) => (
-              <>
-                <FormItem>
-                  <FormLabel>Deposite</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      min={1}
-                      max={10}
-                      step={0.1}
-                      placeholder="0.35"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              </>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="accountId"
-            render={({ field }) => (
-              <FormItem itemType="number" className="w-full">
-                <FormLabel>Select Sub Account</FormLabel>
-                <FormControl>
-                  <Select
-                    {...field}
-                    onValueChange={field.onChange}
-                    value={field.value?.toString()}
-                  >
-                    <SelectTrigger className="w-full" id="accountId">
-                      <SelectValue placeholder="0" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      {clientContext &&
-                        clientContext.subIds?.map((ids) => (
-                          <SelectItem key={ids} value={ids.toString()}>
-                            {ids.toString() || "0"}
-                          </SelectItem>
-                        ))}
-                    </SelectContent>
-                  </Select>
-                </FormControl>
-                {message && (
-                  <span className="text-green-400 font-medium">{message}</span>
-                )}
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="cursor-pointer">
-            Deposite SOL
-          </Button>
-        </form>
-      </Form>
+      <Card className="space-y-8 min-w-2xs mx-auto bg-gray-50 rounded-xl shadow-gray-500 p-4">
+        <CardHeader>
+          <CardTitle>Deposite</CardTitle>
+          <CardDescription>Make a deposite.</CardDescription>
+        </CardHeader>
+        <>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              // className=" mx-auto bg-gray-50 rounded-xl shadow-gray-500 border shadow-sm p-4"
+            >
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <FormField
+                    control={form.control}
+                    name="amount"
+                    render={({ field }) => (
+                      <>
+                        <FormItem>
+                          <FormLabel>Amount</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="number"
+                              min={1}
+                              max={10}
+                              step={0.1}
+                              placeholder="0.35"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      </>
+                    )}
+                  />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <FormField
+                    control={form.control}
+                    name="accountId"
+                    render={({ field }) => (
+                      <FormItem itemType="number" className="w-full">
+                        <FormLabel>Select Sub Account</FormLabel>
+                        <FormControl>
+                          <Select
+                            {...field}
+                            onValueChange={field.onChange}
+                            value={field.value?.toString()}
+                          >
+                            <SelectTrigger className="w-full" id="accountId">
+                              <SelectValue placeholder="0" />
+                            </SelectTrigger>
+                            <SelectContent position="popper">
+                              {clientContext &&
+                                clientContext.subIds?.map((ids) => (
+                                  <SelectItem key={ids} value={ids.toString()}>
+                                    {ids.toString() || "0"}
+                                  </SelectItem>
+                                ))}
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                        {message && (
+                          <span className="text-green-400 font-medium">
+                            {message}
+                          </span>
+                        )}
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <CardFooter className="flex px-0  space-y-1.5 justify-between">
+                  <Button type="submit" className="cursor-pointer">
+                    Deposite SOL
+                  </Button>
+                </CardFooter>
+              </div>
+            </form>
+          </Form>
+        </>
+      </Card>
       {tx && (
         <p className="text-slate-800 text-center break-all">Tx Id: {tx}</p>
       )}
