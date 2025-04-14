14841c4a09683919f99fc151e708da8240aaf4ad
Abhishek7Tech <abhishektab45@gmail.com>
2025-04-14 23:15:01 +0530

    implement get sub account details using websockets functionality

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,5 +1,13 @@
-import { createClient } from "@/app/utils/createClient";
-import { convertToNumber } from "@drift-labs/sdk";
+import {
+  BulkAccountLoader,
+  convertToNumber,
+  DriftClient,
+  loadKeypair,
+  QUOTE_PRECISION,
+  SpotMarkets,
+  User,
+  Wallet,
+} from "@drift-labs/sdk";
 import { PublicKey } from "@solana/web3.js";
 import { Connection } from "@solana/web3.js";
 import { NextResponse } from "next/server";
@@ -9,7 +17,8 @@ const NETWORK =
 
 export async function POST(req: Request) {
   const body: { pubKey: string } = await req.json();
-  const ueserAccountToRead = new PublicKey(body.pubKey);
+  const userAccountToRead = new PublicKey(body.pubKey);
+
   try {
     const connection = new Connection(NETWORK, "confirmed");
     console.log(
@@ -18,25 +27,90 @@ export async function POST(req: Request) {
     if (!process.env.NEXT_PUBLIC_KEY_PAIR) {
       throw new Error("Key pair not found.");
     }
+    const wallet = new Wallet(loadKeypair(process.env.NEXT_PUBLIC_KEY_PAIR));
 
-    const driftClient = await createClient();
-
-    if (!driftClient) {
-      throw new Error("Failed to create client");
+    if (!wallet) {
+      console.log("Wallet not found");
+      return;
     }
-    await driftClient.subscribe();
-    // const baseAssetAmount = convertToNumber(
-    //   user.getPerpPosition(0)?.baseAssetAmount
-    // );
+    console.log("ADDRESS", wallet.publicKey.toString());
+    const solOracleInfo = SpotMarkets["devnet"].find(
+      (market) => market.marketIndex === 1
+    )!;
 
-    // const address = await user.getUserAccountPublicKey();
-    // console.log("Address", address.toString());
-    // const openOrders = convertToNumber(user.getOpenOrders());
-    // console.log("Open Orders", openOrders);
-    // const accountInfo = { publicAddress: address, baseAssetAmount, openOrders };
+    const bulkAccountLoader = new BulkAccountLoader(
+      connection,
+      "confirmed",
+      500
+    );
+
+    const driftClient = new DriftClient({
+      connection,
+      accountSubscription: {
+        type: "websocket", // Use polling with bulkAccountLoader
+      },
+      wallet,
+      perpMarketIndexes: [0, 1],
+      oracleInfos: [
+        {
+          publicKey: solOracleInfo.oracle,
+          source: solOracleInfo.oracleSource,
+        },
+      ],
+      env: "devnet",
+    });
+
+    await driftClient.subscribe();
+
+    console.log("SUBSCRIBED", await driftClient.isSubscribed);
+
+    const user: User = new User({
+      driftClient,
+      userAccountPublicKey: userAccountToRead,
+      accountSubscription: {
+        type: "websocket", // Use polling with bulkAccountLoader
+      },
+    });
+    // console.log("USER", user.subscribe());
+    // await user.subscribe();
+
+    await user.subscribe();
+
+    if (!user.isSubscribed) {
+      console.log("USER NEVER FETCHED");
+      return;
+    }
+
+    console.log(
+      "User prototype methods:",
+      Object.getOwnPropertyNames(Object.getPrototypeOf(user))
+    );
+    console.log(
+      "ACC",
+      await user.subscribe(),
+      convertToNumber(user.getPerpPosition(0)?.quoteAssetAmount)
+    );
+    // console.log(
+    //   "ACC",
+    //   await convertToNumber(user.getUserAccount())
+    // );
+    // const netAccountBalanceBN = await user.getNetUsdValue();
+    // const netAccountBalance = +convertToNumber(netAccountBalanceBN).toFixed(2);
+    // console.log("Base asset", netAccountBalance);
+    // const balance = await user
+    // console.log("Balnance", balance);
+    const address = await user.getUserAccountPublicKey();
+    console.log("Address", address.toString());
+    const openOrders = user.getOpenOrders();
+    console.log("Open Orders", openOrders.length);
+    const accountInfo = {
+      publicAddress: address,
+      //   baseAssetAmount,
+      openOrders: openOrders.length,
+    };
     return NextResponse.json({
       message: "User Account",
-    //   accountInfo,
+      accountInfo,
     });
   } catch (error) {
     console.log("error", error);
